f8772f69f4c3a2f8bd2428d9539148fa81d88319
Amnaun <gong3264513407@outlook.com>
2025-04-04 14:42:54 +0800

    update in 2025/4/4 14:42

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,7 @@ require("Amnaun.lazy")
 vim.g.neovide_curor_vfx_mode = "railgun" --设置主题
 vim.g.neovide_cursor_vfx_opacity = 200 --设置粒子透明度
 vim.g.neovide_refresh_rate = 165 --设置刷新率
-vim.g.neovide_cursor_vfx_particle_size = 2.0 --设置光标宽度
+vim.g.neovide_cursor_vfx_particle_size = 1.0 --设置光标宽度
 vim.g.neovide_cursor_vfx_particle_lifetime = 3500 --设置光标粒子存在时间
 vim.g.neovide_cursor_vfx_particle_speed = 2.5 --粒子扩散速度
 
